7c83e0d689895ad5bc2e93a6373fe5b91211d976
tamer <tamer.americo@gmail.com>
2022-08-16 21:39:22 -0300

    initialize repo webserver nodejs course 2022

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require ('express');
+var app = express();
+var cont = 1;
+var contAbout = 1;
+
+var PORT = 3000;
+
+
+
+
+
+var middleware = {
+
+    requireAuthentication: function (req, res, next){
+        console.log ('processo auth');
+        next();
+    },
+    
+    logger: function (req, res, next){
+        console.log (''+ new Date().toString() + 'Request: ' + req.method +   ' '  + req.originalUrl);
+        next();
+    }
+
+
+
+}
+
+app.use(middleware.logger);
+
+app.get('/', function(rq, rs){
+
+    rs.send('teste / ok');
+    console.log('oi' + cont++);
+
+
+});
+
+
+
+app.get('/about', middleware.requireAuthentication, function(rq, rs){
+
+    rs.send('teste about ok');
+    console.log('about' + contAbout++);
+
+
+});
+
+// expor um folder 
+console.log(__dirname);  // verificar o diretorio que o server está rodando
+app.use(express.static ( __dirname + '/public')   ); // modifica o diretorio para o express
+
+
+
+// app.listen(3000);  // modo basico para executar o server
+
+app.listen(PORT, function(){    // outro formado usando funcao anonima
+        console.log('running web at %s', PORT);
+
+    }
+    );  
